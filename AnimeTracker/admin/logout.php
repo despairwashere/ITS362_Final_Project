2d582a320648431d8366dafac587412ff732064a
roger3555 <roger355@pnw.edu>
2025-04-26 20:35:02 -0500

    Test to see if my dumbass shit works

--- AnimeTracker/admin/logout.php
+++ AnimeTracker/admin/logout.php
@@ -2,6 +2,8 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: ../login/adminlogin.php");
+header("Location: ../login/adminlogin.php"); // Redirect to login page after logout
 exit();
 ?> 
+
+
